a9bcfb85c87c39f849a60e9dab9682fffb0bebd5
Eric Lai <jlai38@ucsc.edu>
2024-03-05 17:55:34 -0800

    17:55 Lai

--- Draft_code.R
+++ Draft_code.R
@@ -1,6 +1,6 @@
 
 
-setwd("/Users/laix1/OneDrive/Desktop/Final_Proposal")
+setwd("/Users/laix1/OneDrive/Desktop/Final_Proposal") #Alter when needed 
 library(haven)
 
 covid_data <- data.frame(read_dta("C19CS Data.dta"))
